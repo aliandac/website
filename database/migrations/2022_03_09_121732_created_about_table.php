b45c39f5c3366ac6df4d197a42e227f643001b21
andy <yazilimofisim@gmail.com>
2022-03-10 15:03:44 +0300

    db migration file

---
+++ database/migrations/2022_03_09_121732_created_about_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatedAboutTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->json('property');
+            $table->text('description');
+            $table->string('images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('abouts');
+    }
+}
